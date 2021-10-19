49ec3fe9cef98dc8b6195a0e92f60b1bb6497371
Vitalii Kemeniash <Vitalii_Kemeniash@epam.com>
2021-10-20 01:09:35 +0300

    Add user microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
+  has_many :microposts, dependent: :destroy
   attr_accessor :remember_token, :activation_token, :reset_token
+
   before_save :downcase_email
   before_create :create_activation_digest
   has_secure_password
@@ -31,6 +33,7 @@ class User < ApplicationRecord
   def authenticated?(attribute, token)
     digest = send("#{attribute}_digest")
     return false if digest.nil?
+
     BCrypt::Password.new(digest).is_password?(token)
   end
 
@@ -64,6 +67,10 @@ class User < ApplicationRecord
 
   def password_reset_expired?
     reset_sent_at < 2.hours.ago
+  end
+
+  def feed
+    Micropost.where('user_id = ?', id)
   end
 
   private
--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -1,4 +1,4 @@
-require "test_helper"
+require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
 
@@ -6,26 +6,26 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     @user = users(:michael)
   end
 
-  test "layout links" do
+  test 'layout links' do
     get root_path
     assert_template 'static_pages/home'
-    assert_select "a[href=?]", root_path, count: 2
-    assert_select "a[href=?]", help_path
-    assert_select "a[href=?]", about_path
-    assert_select "a[href=?]", contact_path
+    assert_select 'a[href=?]', root_path, count: 2
+    assert_select 'a[href=?]', help_path
+    assert_select 'a[href=?]', about_path
+    assert_select 'a[href=?]', contact_path
     get contact_path
-    assert_select "title", full_title("Contact")
+    assert_select 'title', full_title('Contact')
     get signup_path
-    assert_select "title", full_title('Sign up')
+    assert_select 'title', full_title('Sign up')
   end
 
-  test "layout links for logged in user" do
+  test 'layout links for logged in user' do
     log_in_as(@user)
     get root_path
     assert_template 'static_pages/home'
-    assert_select "a[href=?]", users_path
-    assert_select "a[href=?]", edit_user_path(@user)
-    assert_select "a[href=?]", logout_path
+    assert_select 'a[href=?]', users_path
+    assert_select 'a[href=?]', edit_user_path(@user)
+    assert_select 'a[href=?]', logout_path
   end
 
 end
