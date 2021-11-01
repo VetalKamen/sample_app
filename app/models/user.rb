e0606bbfddefba0ae1b6aefa16ed08d1b01224b6
Vitalii Kemeniash <Vitalii_Kemeniash@epam.com>
2021-11-01 20:57:27 +0200

    Rails admin fixed Relationship Dashboard

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,22 +1,23 @@
 class User < ApplicationRecord
   has_many :microposts, dependent: :destroy
   has_many :active_relationships, class_name: 'Relationship',
-           foreign_key: 'follower_id',
-           dependent: :destroy
+                                  foreign_key: 'follower_id',
+                                  dependent: :destroy
   has_many :passive_relationships, class_name: 'Relationship',
-           foreign_key: 'followed_id',
-           dependent: :destroy
+                                   foreign_key: 'followed_id',
+                                   dependent: :destroy
   has_many :following, through: :active_relationships, source: :followed
   has_many :followers, through: :passive_relationships, source: :follower
   attr_accessor :remember_token, :activation_token, :reset_token
+
   before_save :downcase_email
   before_create :create_activation_digest
   has_secure_password
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :name, presence: true, length: { maximum: 50 }
   validates :email, presence: true, length: { maximum: 255 },
-            format: { with: VALID_EMAIL_REGEX },
-            uniqueness: true
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: true
   validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
 
   # Returns the hash digest of the given string.
